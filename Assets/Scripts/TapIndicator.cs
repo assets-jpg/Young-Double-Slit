b3769f1e6cc54e259365c18bc7b2e69873b06188
assets-jpg <assets@metabookxr.com>
2026-01-05 11:53:49 +0530

    bug fixes
    bug fixes

---
+++ Assets/Scripts/TapIndicator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TapIndicator : MonoBehaviour
+{
+    public float minScale = 0.003f;
+    public float maxScale = 0.0036f;
+    public float speed = 2f;
+
+    private RectTransform rectTransform;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        float t = (Mathf.Sin(Time.time * speed) + 1f) * 0.5f;
+        float scale = Mathf.Lerp(minScale, maxScale, t);
+
+        rectTransform.localScale = new Vector3(scale, scale, 1f);
+    }
+}
